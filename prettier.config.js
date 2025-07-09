33772166195caf55031d14c2ba56205955bbaf95
George Bockari <bit@revisioned.consulting>
2025-07-09 11:58:35 -0400

    chore: update prettier configuration to include tailwindcss plugin

--- prettier.config.js
+++ prettier.config.js
@@ -32,5 +32,6 @@ export default {
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["tv", "cx", "cn", "clsx"],
 }
