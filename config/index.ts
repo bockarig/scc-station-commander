062c966c27515c39f047343849cee0fbbc862b2a
George Bockari <bit@revisioned.consulting>
2025-07-09 00:58:12 -0400

    chore: init

---
+++ config/index.ts
@@ -0,0 +1,17 @@
+export const EXTENSION_CONFIG = {
+  name: "SC Commander",
+  description: "A better Station Command. SCC at your fingertips.",
+  // TODO: Add more configuration options as needed
+  // default_locale: "en",
+  // defaultTheme: "system",
+}
+
+export const DEFAULT_USER_CONFIG = {
+  alias: "bockarig",
+  homeFacility: "DGT8",
+  role: "Process Assistant",
+  firstName: "George",
+  lastName: "Bockari",
+  shiftHours: "12:20AM-12:50PM",
+  shiftDays: ["Thursday", "Friday", "Saturday"],
+}
---
+++ wxt.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "wxt"
+
+import { EXTENSION_CONFIG } from "./config"
+
+const isDevelopment = process.env.NODE_ENV === "development"
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-react"],
+  entrypointsDir: "extension",
+  publicDir: "static",
+  webExt: {
+    disabled: true,
+  },
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
+  manifest: {
+    name: isDevelopment ? `DEV | ${EXTENSION_CONFIG.name}` : EXTENSION_CONFIG.name,
+    description: EXTENSION_CONFIG.description,
+    permissions: ["storage"],
+  },
+})
