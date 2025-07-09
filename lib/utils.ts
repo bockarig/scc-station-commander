34c0ba460e6ea9e7b89629231cfed077f57de707
George Bockari <bit@revisioned.consulting>
2025-07-09 12:29:08 -0400

    chore: add style utilities for Tailwind CSS integration

---
+++ lib/utils.ts
@@ -0,0 +1,5 @@
+//*
+// FIXME: This is just for compatibility with the shadcn/tremor ecosystem.
+//  Remove once all imports are updated to use `@/lib/style-utils` instead of `@/lib/utils`.
+// */.
+export * from './style-utils'
